558e2b8b7743edfec32be1c677a0286c6d8a853c
jakub-bartoszek-developer <k.bartoszek507@wp.pl>
2023-12-01 15:34:30 +0100

    added height and flexbox to nav

--- src/components/Navigation/styled.jsx
+++ src/components/Navigation/styled.jsx
@@ -3,14 +3,19 @@ import styled from "styled-components";
 
 export const Wrapper = styled.nav`
  width: 100%;
+ height: 72px;
  background-color: black;
  color: white;
 `;
 
 export const Container = styled.div`
+ height: 100%;
  margin: 0 auto;
  max-width: 1400px;
  color: white;
+ display: flex;
+ align-items: center;
+ justify-content: space-around;
 `;
 
 export const StyledLink = styled(Link)`
