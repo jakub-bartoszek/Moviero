89b59a6e1caa51814d89e6e96b1eedf25a95e26a
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-04 13:53:46 +0100

    created logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,5 @@
+import { Wrapper } from "./styled";
+
+export const Logo = () => {
+ return <Wrapper>Moviero</Wrapper>;
+};
---
+++ src/components/Logo/styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ font-size: 32px;
+ font-weight: 700;
+ color: rgb(0, 255, 255);
+ text-shadow: 0 0 15px rgba(0, 255, 255, 0.5);
+`;
