4e361611f0cb5a70a848a2e884252478d79ac0d4
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-04 19:50:52 +0100

    fetching popular movies

--- src/utils/saga/dataSaga.js
+++ src/utils/saga/dataSaga.js
@@ -1,7 +1,15 @@
-import { takeEvery } from "@redux-saga/core/effects";
-import { fetchPopular } from "../redux/dataSlice";
+import { takeEvery, call, put } from "@redux-saga/core/effects";
+import { fetchPopular, setData } from "../redux/dataSlice";
+import { getPopular } from "./getPopular";
 
 function* fetchPopularHandler() {
+ try {
+  const data = yield call(getPopular);
+  yield put(setData(data));
+ }
+ catch (error) {
+  console.log(error);
+ }
 }
 
 export function* dataSaga() {
