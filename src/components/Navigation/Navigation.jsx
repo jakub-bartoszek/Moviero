f6bf59e0d06d1f5fff920074e5cdb5fd54b5476e
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-04 13:54:06 +0100

    styles for navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,18 @@
-import { Container, StyledLink, Wrapper } from "./styled";
+import { Logo } from "../Logo/Logo";
+import { Searchbar } from "../Searchbar/Searchbar";
+import { Container, Links, StyledLink, Wrapper } from "./styled";
 
 export const Navigation = () => {
  return (
   <Wrapper>
    <Container>
-    <StyledLink to="/movies">Movies</StyledLink>
-    <StyledLink to="/people">People</StyledLink>
+    <Logo />
+    <Links>
+     <StyledLink to="/movies">Movies</StyledLink>
+     <StyledLink to="/people">People</StyledLink>
+     <StyledLink to="/people">TV Shows</StyledLink>
+    </Links>
+    <Searchbar />
    </Container>
   </Wrapper>
  );
--- src/components/Navigation/styled.jsx
+++ src/components/Navigation/styled.jsx
@@ -3,17 +3,42 @@ import styled from "styled-components";
 
 export const Wrapper = styled.nav`
  width: 100%;
- background-color: black;
+ background: rgb(0, 34, 44);
+ background: linear-gradient(
+  0deg,
+  rgba(0, 34, 44, 0.4013921113689095) 0%,
+  rgba(0, 34, 44, 1) 100%
+ );
+ height: 72px;
  color: white;
+ border-bottom: 2px solid #ffffff30;
 `;
 
 export const Container = styled.div`
  margin: 0 auto;
+ height: 100%;
  max-width: 1400px;
+ padding: 0 16px;
  color: white;
+ display: grid;
+ align-items: center;
+ justify-content: space-between;
+	gap: 16px;
+ grid-template-columns: min-content auto minmax(auto, 33%);
+`;
+
+export const Links = styled.div`
+ display: flex;
+ justify-content: space-around;
+ gap: 32px;
+ align-items: center;
 `;
 
 export const StyledLink = styled(Link)`
  text-decoration: none;
  color: inherit;
+ font-size: 16px;
+ font-weight: 400;
+ padding: 16px;
+ color: white;
 `;
