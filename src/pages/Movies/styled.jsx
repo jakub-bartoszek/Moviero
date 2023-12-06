9e83f4ac11d4ddb025bf38860357e320b4aad026
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-06 19:29:40 +0100

    styles for homepage

--- src/pages/Movies/styled.jsx
+++ src/pages/Movies/styled.jsx
@@ -1,61 +1,180 @@
 import styled from "styled-components";
+import { ReactComponent as Star } from "../../assets/icons/star.svg";
 
-export const BannerWrapper = styled.div`
+export const Banner = styled.div`
  box-shadow: inset 0px 0px 80px 100px #00141a, inset 0px 0px 50px 50px #00141a;
  width: 100%;
- height: 90vw;
+ height: 100vw;
  max-height: 600px;
+ background-image: ${({ $bgImage }) => `url(${$bgImage})`};
+ background-size: cover;
+ background-position: center;
+ color: white;
 `;
 
-export const Banner = styled.img`
- border-radius: 50px;
- position: relative;
+export const MovieDetails = styled.div`
+ padding-top: 82px;
  width: 100%;
+ display: flex;
+ flex-direction: column;
+ gap: 16px;
  height: 100%;
- object-fit: cover;
- object-position: top;
- z-index: -1;
- max-height: 600px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  gap: 8px;
+ }
+`;
+
+export const Title = styled.h1`
+ font-size: 72px;
+ padding: 0;
+ margin: 0;
+ text-shadow: 0 0 10px black, 0 0 20px black;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.l}) {
+  font-size: 64px;
+ }
+ @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+  font-size: 52px;
+ }
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 40px;
+ }
+ @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+  font-size: 24px;
+ }
+`;
+
+export const Genres = styled.div`
+ display: flex;
+ flex-wrap: wrap;
+ gap: 8px;
+ width: 50%;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  gap: 4px;
+ }
+`;
+
+export const Genre = styled.div`
+ background-color: #00000070;
+ padding: 6px;
+ font-size: 12px;
+ border-radius: 5px;
+ text-shadow: 0 0 5px black, 0 0 10px black, 0 0 20px black, 0 0 30px black;
+ backdrop-filter: blur(10px);
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  padding: 4px;
+  font-size: 10px;
+ }
+`;
+
+export const Rating = styled.div`
+ display: flex;
+ gap: 8px;
+ align-items: center;
+ text-shadow: 0 0 5px black, 0 0 10px black, 0 0 20px black, 0 0 30px black;
+`;
+
+export const StarIcon = styled(Star)`
+ color: yellow;
+ height: 24px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  height: 16px;
+ }
+`;
+
+export const Rate = styled.span`
+ font-size: 24px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 16px;
+ }
+`;
+
+export const OutOf = styled.span`
+ font-size: 16px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 10px;
+ }
+`;
+
+export const Votes = styled.span`
+ font-weight: 300;
+ color: #d1d1d1;
+ font-size: 16px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 10px;
+ }
+`;
+
+export const Description = styled.p`
+ padding: 6px;
+ width: 50%;
+ overflow-y: auto;
+ border-radius: 10px;
+ background-color: #00000050;
+ font-size: 16px;
+ font-weight: 300;
+ backdrop-filter: blur(5px);
+ text-shadow: 0 0 5px black, 0 0 5px black, 0 0 5px black, 0 0 5px black;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 12px;
+  width: 100%;
+  margin-top: auto;
+  margin-bottom: 0;
+ }
+`;
+
+export const SimilarMoviesWrapper = styled.section`
+ max-height: 300px;
+ height: 55vw;
+ margin-top: 12px;
+`;
+
+export const SectionHeader = styled.h2`
+ color: white;
+ margin: 0;
+ padding: 0;
 `;
 
 export const SimilarMovies = styled.ul`
  list-style-type: none;
  display: flex;
- padding: 16px 16px 16px 0;
+ padding: 8px 0;
  gap: 16px;
  overflow-y: hidden;
- height: 300px;
  align-items: center;
  position: relative;
  scroll-behavior: smooth;
-
- &::-webkit-scrollbar {
-  height: 10px;
- }
-
- &::-webkit-scrollbar-track {
-  background: transparent;
- }
-
- &::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 50px;
- }
-
- &::-webkit-scrollbar-thumb:hover {
-  background: #555;
- }
+ height: 100%;
 `;
 
 export const PosterWrapper = styled.li`
  height: 95%;
  transition: all 0.3s;
+ border-radius: 6px;
+ transform-origin: bottom center;
 
  &:hover {
-  height: 100%;
+  scale: 105%;
+ }
+
+ &:first-child {
+  transform-origin: bottom left;
+ }
+
+ &:last-child {
+  transform-origin: bottom right;
  }
 `;
 
 export const Poster = styled.img`
  height: 100%;
+ border-radius: inherit;
 `;
