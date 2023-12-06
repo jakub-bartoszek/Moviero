9cfbcebbd28056015740f5414f8533e94e0a3c32
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-06 19:27:36 +0100

    added theme

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import { GlobalStyle } from './styles/GlobalStyle';
 import { Normalize } from 'styled-normalize';
 import store from './utils/redux/store';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <ThemeProvider theme={theme}>
     <StrictMode>
       <GlobalStyle />
       <Normalize />
       <App />
     </StrictMode>
+    </ThemeProvider>
   </Provider>
 );
 
---
+++ src/styles/theme.jsx
@@ -0,0 +1,21 @@
+const colorNames = {
+ White: "#FFFFFF",
+ Black: "#000000",
+};
+
+export const theme = {
+ breakpoints: {
+   xs: "400px",
+   s: "600px",
+   m: "900px",
+   l: "1100px",
+   xl: "1400px",
+ },
+
+ colors: {
+   site: {
+     background: colorNames.Whisper,
+     text: colorNames.Black,
+   },
+ },
+};
