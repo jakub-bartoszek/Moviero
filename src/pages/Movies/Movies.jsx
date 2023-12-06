0fae5a0a7eed678d0bbea39719c8d515b4de8d15
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-06 19:28:47 +0100

    fetching movies, and displaying info

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,19 +1,46 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "../../components/Container/styled";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPopular, selectData } from "../../utils/redux/dataSlice";
+import {
+ fetchPopularMovies,
+ fetchSimilarMovies,
+ selectGenres,
+ selectPopularMovies,
+ selectRandomPopularMovie,
+ selectSimilarMovies,
+ setRandomPopularMovie
+} from "../../utils/redux/moviesSlice";
 import {
  Banner,
- BannerWrapper,
+ Description,
+ Genre,
+ Genres,
+ MovieDetails,
+ OutOf,
  Poster,
  PosterWrapper,
- SimilarMovies
+ Rate,
+ Rating,
+ SectionHeader,
+ SimilarMovies,
+ SimilarMoviesWrapper,
+ StarIcon,
+ Title,
+ Votes
 } from "./styled";
+import { nanoid } from "nanoid";
 
 export const Movies = () => {
  const dispatch = useDispatch();
- const movies = useSelector(selectData);
- const [currentMovie, setCurrentMovie] = useState(movies[0] || {});
+ const popularMovies = useSelector(selectPopularMovies);
+ const similarMovies = useSelector(selectSimilarMovies);
+ const genres = useSelector(selectGenres);
+ const [currentMovie, setCurrentMovie] = useState(popularMovies[0]);
+ const randomPopularMovie = useSelector(selectRandomPopularMovie);
+ const movieGenres = genres?.filter((genre) =>
+  currentMovie?.genre_ids.includes(genre.id)
+ );
+ const [cancel, setCancel] = useState(false);
 
  const handleClickScroll = (e) => {
   const element = e.target;
@@ -23,36 +50,82 @@ export const Movies = () => {
  };
 
  useEffect(() => {
-  dispatch(fetchPopular());
- }, []);
+  dispatch(fetchPopularMovies());
+ }, [dispatch]);
 
  useEffect(() => {
-  setCurrentMovie(movies[0]);
- }, [movies]);
+  if (popularMovies.length > 0) {
+   dispatch(
+    setRandomPopularMovie(popularMovies[Math.floor(Math.random() * 19)])
+   );
+  }
+ }, [dispatch, popularMovies]);
+
+ useEffect(() => {
+  if (Object.keys(randomPopularMovie).length > 0) {
+   setCurrentMovie(randomPopularMovie);
+  }
+ }, [dispatch, randomPopularMovie]);
+
+ useEffect(() => {
+  if (!cancel && currentMovie && Object.keys(currentMovie).length > 0) {
+   dispatch(
+    fetchSimilarMovies({ genreIds: currentMovie.genre_ids.join("%2C") })
+   );
+   setCancel(true);
+  }
+ }, [cancel, currentMovie, dispatch]);
+
+ useEffect(() => {
+  if (similarMovies.length > 0) {
+  }
+ }, [dispatch, similarMovies]);
 
  return (
   <Container>
-   <BannerWrapper>
+   {currentMovie && (
     <Banner
-     src={`https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}`}
-    />
-   </BannerWrapper>
-   <SimilarMovies>
-    {movies.map((movie) => (
-     <PosterWrapper
-      onClick={(e) => {
-       handleClickScroll(e);
-       setCurrentMovie(movie);
-      }}
-      key={movie.id}
-     >
-      <Poster
-       id={movie.id}
-       src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-      />
-     </PosterWrapper>
-    ))}
-   </SimilarMovies>
+     $bgImage={`https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}`}
+    >
+     <MovieDetails>
+      <Title>{currentMovie?.title}</Title>
+      <Genres>
+       {movieGenres.map((genre) => (
+        <Genre key={nanoid()}>{genre.name}</Genre>
+       ))}
+      </Genres>
+      <Rating>
+       <StarIcon />
+       <Rate>
+        {currentMovie?.vote_average}/<OutOf>10</OutOf>
+        <Votes>&nbsp;{currentMovie?.vote_count} votes</Votes>
+       </Rate>
+      </Rating>
+      <Description>{currentMovie?.overview}</Description>
+     </MovieDetails>
+    </Banner>
+   )}
+   {similarMovies.length > 0 && (
+    <SimilarMoviesWrapper>
+     <SectionHeader>Similar movies</SectionHeader>
+     <SimilarMovies>
+      {similarMovies.map((movie) => (
+       <PosterWrapper
+        key={nanoid()}
+        onClick={(e) => {
+         handleClickScroll(e);
+         setCurrentMovie(movie);
+        }}
+       >
+        <Poster
+         id={movie.id}
+         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        />
+       </PosterWrapper>
+      ))}
+     </SimilarMovies>
+    </SimilarMoviesWrapper>
+   )}
   </Container>
  );
 };
