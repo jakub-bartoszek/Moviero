3ad13c9f3aa0d1b9918b05795468ab44e32e3923
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-07 18:58:10 +0100

    render depending on the status

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Container } from "../../components/Container/styled";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -8,6 +8,7 @@ import {
  selectPopularMovies,
  selectRandomPopularMovie,
  selectSimilarMovies,
+ selectStatus,
  setRandomPopularMovie
 } from "../../utils/redux/moviesSlice";
 import {
@@ -32,9 +33,13 @@ import {
 } from "./styled";
 import { nanoid } from "nanoid";
 import { Tile } from "../../components/Tile/Tile";
+import { useSearchParams } from "react-router-dom";
+import { Pagination } from "../../components/Pagination/Pagination";
+import { Loader } from "../../components/Loader/Loader";
 
 export const Movies = () => {
  const dispatch = useDispatch();
+ const status = useSelector(selectStatus);
  const popularMovies = useSelector(selectPopularMovies);
  const similarMovies = useSelector(selectSimilarMovies);
  const genres = useSelector(selectGenres);
@@ -44,6 +49,9 @@ export const Movies = () => {
   currentMovie?.genre_ids.includes(genre.id)
  );
  const [cancel, setCancel] = useState(false);
+ const [searchParams, setSearchParams] = useSearchParams();
+ const containerRef = useRef(null);
+ const bannerRef = useRef(null);
 
  const handleClickScroll = (e) => {
   const element = e.target;
@@ -51,14 +59,23 @@ export const Movies = () => {
    element.scrollIntoView({
     behavior: "smooth",
     inline: "center",
-    block: "end"
+    block: "nearest"
    });
   }
  };
 
  useEffect(() => {
-  dispatch(fetchPopularMovies());
- }, [dispatch]);
+  if (!searchParams.get("page")) {
+   searchParams.set("page", 1);
+   setSearchParams(searchParams);
+  }
+ }, []);
+
+ useEffect(() => {
+  if (searchParams.get("page")) {
+   dispatch(fetchPopularMovies({ page: searchParams.get("page") }));
+  }
+ }, [searchParams, dispatch]);
 
  useEffect(() => {
   if (popularMovies.length > 0) {
@@ -71,6 +88,7 @@ export const Movies = () => {
  useEffect(() => {
   if (Object.keys(randomPopularMovie).length > 0) {
    setCurrentMovie(randomPopularMovie);
+   setCancel(false);
   }
  }, [dispatch, randomPopularMovie]);
 
@@ -84,63 +102,79 @@ export const Movies = () => {
  }, [cancel, currentMovie, dispatch]);
 
  return (
-  <Container>
-   {currentMovie && (
-    <Banner
-     $bgImage={`https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}`}
-    >
-     <MovieDetails>
-      <Title>{currentMovie?.title}</Title>
-      <Genres>
-       {movieGenres.map((genre) => (
-        <Genre key={nanoid()}>{genre.name}</Genre>
-       ))}
-      </Genres>
-      <Rating>
-       <StarIcon />
-       <Rate>
-        {currentMovie?.vote_average}/<OutOf>10</OutOf>
-        <Votes>&nbsp;{currentMovie?.vote_count} votes</Votes>
-       </Rate>
-      </Rating>
-      <Description>{currentMovie?.overview}</Description>
-     </MovieDetails>
-    </Banner>
-   )}
-   {similarMovies.length > 0 && (
-    <SimilarMoviesWrapper>
-     <SectionHeader>Similar movies</SectionHeader>
-     <SimilarMovies>
-      {similarMovies.map((movie) => (
-       <PosterWrapper
-        $current={movie.id === currentMovie.id}
-        key={nanoid()}
-        onClick={(e) => {
-         handleClickScroll(e);
-         setCurrentMovie(movie);
-        }}
-       >
-        <Poster
-         id={movie.id}
-         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        />
-       </PosterWrapper>
-      ))}
-     </SimilarMovies>
-    </SimilarMoviesWrapper>
-   )}
-   <PopularMoviesWrapper>
-    <SectionHeader>Popular movies</SectionHeader>
-    <PopularMovies>
-     {popularMovies.map((movie) => (
-      <Tile
-       genres={genres}
-       key={nanoid()}
-       movie={movie}
-      />
-     ))}
-    </PopularMovies>
-   </PopularMoviesWrapper>
+  <Container ref={containerRef}>
+   {
+    {
+     loading: <Loader />,
+     error: <>Error</>,
+     success: (
+      <>
+       {currentMovie && (
+        <Banner
+         key={currentMovie.id}
+         $bgImage={`https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}`}
+        >
+         <MovieDetails>
+          <Title>{currentMovie?.title}</Title>
+          <Genres>
+           {movieGenres.map((genre) => (
+            <Genre key={nanoid()}>{genre.name}</Genre>
+           ))}
+          </Genres>
+          <Rating>
+           <StarIcon />
+           <Rate>
+            {currentMovie?.vote_average}/<OutOf>10</OutOf>
+            <Votes>&nbsp;{currentMovie?.vote_count} votes</Votes>
+           </Rate>
+          </Rating>
+          <Description>{currentMovie?.overview}</Description>
+         </MovieDetails>
+        </Banner>
+       )}
+       {similarMovies.length > 0 && (
+        <SimilarMoviesWrapper>
+         <SectionHeader>Similar movies</SectionHeader>
+         <SimilarMovies>
+          {similarMovies.map((movie) => (
+           <PosterWrapper
+            $current={movie.id === currentMovie.id}
+            key={nanoid()}
+            onClick={(e) => {
+             handleClickScroll(e);
+             setCurrentMovie(movie);
+            }}
+           >
+            <Poster
+             id={movie.id}
+             src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            />
+           </PosterWrapper>
+          ))}
+         </SimilarMovies>
+        </SimilarMoviesWrapper>
+       )}
+       <PopularMoviesWrapper>
+        <SectionHeader>Popular movies</SectionHeader>
+        <PopularMovies>
+         {popularMovies.map((movie) => (
+          <Tile
+           genres={genres}
+           key={nanoid()}
+           movie={movie}
+          />
+         ))}
+        </PopularMovies>
+       </PopularMoviesWrapper>
+       <Pagination
+        containerRef={containerRef}
+        searchParams={searchParams}
+        setSearchParams={setSearchParams}
+       />
+      </>
+     )
+    }[status]
+   }
   </Container>
  );
 };
