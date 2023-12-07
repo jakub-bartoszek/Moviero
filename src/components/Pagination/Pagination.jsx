4ebe9d5621eb3b1e27df518d72cb952a19d41bb9
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-07 18:57:41 +0100

    created working Pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,72 @@
+import { useSelector } from "react-redux";
+import { selectTotalPages } from "../../utils/redux/moviesSlice";
+import { Container, PageCount, StyledButton } from "./styled";
+
+export const Pagination = ({ searchParams, setSearchParams, containerRef }) => {
+ const totalPages = useSelector(selectTotalPages);
+ const page = parseInt(searchParams.get("page"));
+
+ const handlePageClickScroll = () => {
+  if (containerRef.current) {
+   containerRef.current.scrollIntoView({
+    alignToTop: true
+   });
+  }
+ };
+
+ const onFirstPageHandler = () => {
+  page !== 1 && searchParams.set("page", 1);
+  setSearchParams(searchParams);
+  handlePageClickScroll();
+ };
+
+ const onPreviousPageHandler = () => {
+  page !== 1 && searchParams.set("page", page - 1);
+  setSearchParams(searchParams);
+  handlePageClickScroll();
+ };
+
+ const onNextPageHandler = () => {
+  page !== totalPages && searchParams.set("page", page + 1);
+  setSearchParams(searchParams);
+  handlePageClickScroll();
+ };
+
+ const onLastPageHandler = () => {
+  page !== totalPages && searchParams.set("page", totalPages);
+  setSearchParams(searchParams);
+  handlePageClickScroll();
+ };
+
+ return (
+  <Container>
+   <StyledButton
+    onClick={onFirstPageHandler}
+    disabled={page === 1}
+   >
+    &lt;&lt;
+   </StyledButton>
+   <StyledButton
+    onClick={onPreviousPageHandler}
+    disabled={page === 1}
+   >
+    &lt;
+   </StyledButton>
+   <PageCount>
+    Page {page} of {totalPages}
+   </PageCount>
+   <StyledButton
+    onClick={onNextPageHandler}
+    disabled={page === totalPages}
+   >
+    &gt;
+   </StyledButton>
+   <StyledButton
+    onClick={onLastPageHandler}
+    disabled={page === totalPages}
+   >
+    &gt;&gt;
+   </StyledButton>
+  </Container>
+ );
+};
---
+++ src/components/Pagination/styled.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+ width: 100%;
+ display: flex;
+ gap: 8px;
+ justify-content: center;
+ align-items: center;
+ margin: 24px 0;
+ color: white;
+`;
+
+export const StyledButton = styled.button`
+ color: white;
+ height: 32px;
+ padding: 0 24px;
+ background-color: #00222c;
+ border: none;
+ border-radius: 10px;
+ cursor: pointer;
+ transition: all 0.3s;
+
+ &:hover {
+  scale: 105%;
+  background-color: #003f52;
+ }
+
+ &:disabled {
+  cursor: auto;
+  color: grey;
+  scale: 100%;
+  background-color: #00222c;
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+  padding: 0 26px;
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  height: 20px;
+  padding: 0 12px;
+ }
+`;
+
+export const PageCount = styled.span`
+ font-size: 16px;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+  font-size: 14px;
+ }
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+  font-size: 10px;
+ }
+`
