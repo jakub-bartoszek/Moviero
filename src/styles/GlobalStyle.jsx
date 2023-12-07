73ef85a52f63de9ee84e53e8534c4d1019ff3dfb
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-07 18:58:43 +0100

    removed height of html, body and root element

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -4,8 +4,6 @@ export const GlobalStyle = createGlobalStyle`
   html{
     box-sizing: border-box;
     font-family: 'Poppins', sans-serif;
-    height: 100vh;
-    overflow-x: hidden;
   }
 
   *, ::after, ::before{
@@ -15,13 +13,9 @@ export const GlobalStyle = createGlobalStyle`
 
   body{
     font-family: 'Poppins', sans-serif;
-    height: 100%;
     color: black;
     background-color: #00141a;
-  }
 
-  #root{
-    height: 100%;
   }
 
   &::-webkit-scrollbar {
