3c54ec54667d923480351ee675140ada87baaba2
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-07 18:59:25 +0100

    fetching totalpages from api

--- src/utils/saga/movies/getPopularMovies.js
+++ src/utils/saga/movies/getPopularMovies.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const getPopularMovies = async () => {
- const response = await axios(`https://api.themoviedb.org/3/movie/popular`, {
-  headers: {
-   accept: "application/json",
-   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTNkYzA0NDk2MjU0OTgwZDAxZGE2ZjEyZWNkOGUxZCIsInN1YiI6IjY1NGJiZGEyNDFhNTYxMzM2YzVlZDg2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jjbm7YAkE1x0vuKowvjXJFiE28sDgW1TB_diyZmPKVQ",
-  },
- });
+export const getPopularMovies = async (page) => {
+  const response = await axios(`https://api.themoviedb.org/3/movie/popular?page=${page}`, {
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTNkYzA0NDk2MjU0OTgwZDAxZGE2ZjEyZWNkOGUxZCIsInN1YiI6IjY1NGJiZGEyNDFhNTYxMzM2YzVlZDg2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jjbm7YAkE1x0vuKowvjXJFiE28sDgW1TB_diyZmPKVQ",
+    },
+  });
 
- return await response.data.results;
+  return await response.data;
 };
