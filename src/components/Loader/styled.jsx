2abb8f98f1c45d22b6bd8a12ad967239984c068d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-08 18:38:08 +0100

    changed height to 100%

--- src/components/Loader/styled.jsx
+++ src/components/Loader/styled.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
  display: flex;
- height: 100vh;
+ height: 100%;
  align-items: center;
  justify-content: center;
  width: 100%;
