00e66dc6a41596db1fb4ea989f8513755fa69ed0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-09 16:23:49 +0100

    page variable

--- src/utils/saga/movies/getSearchResults.js
+++ src/utils/saga/movies/getSearchResults.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const getSearchResults = async (searchQuery) => {
- const response = await axios(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}`, {
-  headers: {
-   accept: "application/json",
-   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTNkYzA0NDk2MjU0OTgwZDAxZGE2ZjEyZWNkOGUxZCIsInN1YiI6IjY1NGJiZGEyNDFhNTYxMzM2YzVlZDg2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jjbm7YAkE1x0vuKowvjXJFiE28sDgW1TB_diyZmPKVQ",
-  },
- });
+export const getSearchResults = async (searchQuery, page) => {
+  const response = await axios(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&${page}`, {
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTNkYzA0NDk2MjU0OTgwZDAxZGE2ZjEyZWNkOGUxZCIsInN1YiI6IjY1NGJiZGEyNDFhNTYxMzM2YzVlZDg2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jjbm7YAkE1x0vuKowvjXJFiE28sDgW1TB_diyZmPKVQ",
+    },
+  });
 
- return await response.data.results;
+  return await response.data.results;
 };
