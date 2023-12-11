c03199ba8af0bbdfff0c59158135e57201f8a535
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-11 16:19:36 +0100

    created preson tile

---
+++ src/components/PersonTile/PersonTile.jsx
@@ -0,0 +1,30 @@
+import {
+ Content,
+ Job,
+ Name,
+ Image,
+ ImageWrapper,
+ Role,
+ StyledLink,
+ Wrapper
+} from "./styled";
+import { toPerson } from "../../routes";
+
+export const PersonTile = ({ person }) => {
+ return (
+  <Wrapper>
+   <StyledLink to={toPerson({ id: person.id })}>
+    <ImageWrapper>
+     {person.profile_path && (
+      <Image src={`https://image.tmdb.org/t/p/w400/${person.profile_path}`} />
+     )}
+    </ImageWrapper>
+    <Content>
+     <Name> {person.name}</Name>
+     {person.job && <Job>{person.job}</Job>}
+     {person.character && <Role>{person.character}</Role>}
+    </Content>
+   </StyledLink>
+  </Wrapper>
+ );
+};
---
+++ src/components/PersonTile/styled.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Wrapper = styled.li``;
+
+export const StyledLink = styled(Link)`
+ display: grid;
+ grid-template-rows: auto 1fr;
+ color: white;
+ padding: 0;
+ background-color: rgb(0, 34, 44);
+ border-radius: 5px;
+ transition: all 0.3s;
+ cursor: pointer;
+ height: 100%;
+ text-decoration: none;
+`;
+
+export const ImageWrapper = styled.div`
+ aspect-ratio: 6/9;
+ width: 100%;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ background-color: black;
+ border-radius: 5px 5px 0 0;
+`;
+
+export const Image = styled.img`
+ width: 100%;
+ border-radius: 5px 5px 0 0;
+`;
+
+export const Content = styled.div`
+ display: flex;
+ justify-content: space-between;
+ flex-direction: column;
+ align-items: center;
+ height: 100%;
+ overflow: hidden;
+ padding: 8px;
+ text-align: center;
+
+ @media (max-width: ${({ theme }) => theme.breakpoints.l}) {
+  gap: 6px;
+ }
+`;
+
+export const Name = styled.p``;
+
+export const Role = styled.p`
+ color: grey;
+`;
+
+export const Job = styled.p`
+ color: grey;
+`;
