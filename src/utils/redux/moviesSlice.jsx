bf04d888ebbcb3ed7ab2f2d2dbf3237359b66223
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-17 23:21:15 +0100

    removed movie details

--- src/utils/redux/moviesSlice.jsx
+++ src/utils/redux/moviesSlice.jsx
@@ -7,14 +7,12 @@ const moviesSlice = createSlice({
   popularMovies: [],
   randomPopularMovie: {},
   similarMovies: [],
-  movieDetails: [],
   genres: [],
   totalPages: 1,
  },
  reducers: {
   fetchPopularMovies: () => {},
   fetchSimilarMovies: () => {},
-  fetchMovieDetails: () => {},
   setStatus: (state, { payload }) => {
    state.status = payload;
   },
@@ -27,9 +25,6 @@ const moviesSlice = createSlice({
   },
   setSimilarMovies: (state, { payload }) => {
    state.similarMovies = payload;
-  },
-  setMovieDetails: (state, { payload }) => {
-   state.movieDetails = payload;
   },
   setGenres: (state, { payload }) => {
    state.genres = payload;
@@ -48,10 +43,8 @@ export const {
  fetchPopularMovies,
  setRandomPopularMovie,
  fetchSimilarMovies,
- fetchMovieDetails,
  setPopularMovies,
  setSimilarMovies,
- setMovieDetails,
  setGenres,
  setTotalPages,
  setStatus
@@ -61,7 +54,6 @@ export const selectPopularMovies = (state) => state.movies.popularMovies;
 export const selectRandomPopularMovie = (state) =>
  state.movies.randomPopularMovie;
 export const selectSimilarMovies = (state) => state.movies.similarMovies;
-export const selectMovieDetails = (state) => state.movies.movieDetails;
 export const selectGenres = (state) => state.movies.genres;
 export const selectTotalPages = (state) => state.movies.totalPages;
 export const selectStatus = (state) => state.movies.status;
