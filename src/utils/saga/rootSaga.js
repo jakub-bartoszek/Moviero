98b3d7abb2b09f874b8249e9bb2e3011d1711863
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-17 17:01:54 +0100

    created searchSaga

--- src/utils/saga/rootSaga.js
+++ src/utils/saga/rootSaga.js
@@ -3,7 +3,8 @@ import { all } from "redux-saga/effects";
 import { moviesSaga } from "./movies/moviesSaga";
 import { movieSaga } from "./movies/movieSaga";
 import { peopleSaga } from "./people/peopleSaga";
+import { searchSaga } from "./search/searchSaga";
 
 export default function* rootSaga() {
-  yield all([moviesSaga(), movieSaga(), peopleSaga()]);
+  yield all([moviesSaga(), movieSaga(), peopleSaga(), searchSaga()]);
 }
