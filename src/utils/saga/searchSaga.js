dca0949d1d3833a1ea25d7d0601f25f31cd75779
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-25 17:37:00 +0100

    debounced searching

--- src/utils/saga/searchSaga.js
+++ src/utils/saga/searchSaga.js
@@ -1,36 +1,33 @@
-import { takeLatest, call, put, delay } from "@redux-saga/core/effects";
+import { takeLatest, call, put, delay, debounce } from "@redux-saga/core/effects";
 import { fetchSearchbarResults, fetchSearchResults, setSearchbarResults, setSearchbarStatus, setSearchResults, setSearchStatus, setTotalPages, setTotalResults } from "../redux/searchSlice";
 import { fetchAPI } from "../fetchAPI";
 
 function* fetchSearchbarResultsHandler({ payload }) {
- try {
-  yield put(setSearchbarStatus("loading"));
-  const searchbarResults = yield call(fetchAPI, `search/${payload.category}?query=${payload.searchQuery}&page=${payload.page}`);
-  yield put(setSearchbarResults(searchbarResults.results));
-  yield delay(200);
-  yield put(setSearchbarStatus("success"));
- }
- catch (error) {
-  yield put(setSearchbarStatus("error"));
- }
+  try {
+    yield put(setSearchbarStatus("loading"));
+    const searchbarResults = yield call(fetchAPI, `search/${payload.category}?query=${payload.searchQuery}&page=${payload.page}`);
+    yield put(setSearchbarResults(searchbarResults.results));
+    yield put(setSearchbarStatus("success"));
+  } catch (error) {
+    yield put(setSearchbarStatus("error"));
+  }
 }
 
 function* fetchSearchResultsHandler({ payload }) {
- try {
-  yield put(setSearchStatus("loading"));
-  const searchResults = yield call(fetchAPI, `search/${payload.category}?query=${payload.searchQuery}&page=${payload.page}`);
-  yield put(setSearchResults(searchResults.results));
-  yield put(setTotalPages(searchResults.total_pages));
-  yield put(setTotalResults(searchResults.total_results));
-  yield delay(500);
-  yield put(setSearchStatus("success"));
- }
- catch (error) {
-  yield put(setSearchStatus("error"));
- }
+  try {
+    yield put(setSearchStatus("loading"));
+    const searchResults = yield call(fetchAPI, `search/${payload.category}?query=${payload.searchQuery}&page=${payload.page}`);
+    yield put(setSearchResults(searchResults.results));
+    yield put(setTotalPages(searchResults.total_pages));
+    yield put(setTotalResults(searchResults.total_results));
+    yield delay(300);
+    yield put(setSearchStatus("success"));
+  } catch (error) {
+    yield put(setSearchStatus("error"));
+  }
 }
 
 export function* searchSaga() {
- yield takeLatest(fetchSearchbarResults.type, fetchSearchbarResultsHandler);
- yield takeLatest(fetchSearchResults.type, fetchSearchResultsHandler);
+  yield debounce(300, fetchSearchbarResults.type, fetchSearchbarResultsHandler);
+  yield takeLatest(fetchSearchResults.type, fetchSearchResultsHandler);
 }
