fa09a1128e082ddc120549e3604cd8614de63fec
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-15 13:48:33 +0200

    changed width of Logo

--- src/components/Logo/styled.jsx
+++ src/components/Logo/styled.jsx
@@ -2,15 +2,11 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Wrapper = styled(NavLink)`
- display: flex;
- padding: 0;
- margin: 0;
- justify-content: start;
- align-items: center;
+ width: min-content;
  font-size: 32px;
  font-weight: 700;
  color: ${({ theme }) => theme.colors.site.primaryText};
- text-shadow: 0 0 15px ${({ theme }) => theme.colors.site.primaryText};;
+ text-shadow: 0 0 15px ${({ theme }) => theme.colors.site.primaryText};
  text-decoration: none;
 
  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
